dd0f6a763a4e7d8185c517943d85de2bd394c6dd
Christopher Kuhn <135048573+BambyQT@users.noreply.github.com>
2024-04-09 15:06:53 +0200

    Updated data
    Implement Code to Model, Controller & Routes

--- startup/startRoutes.js
+++ startup/startRoutes.js
@@ -1,10 +1,12 @@
 import {testRouter} from '../routes/testRouter.js';
 import {toursRouter} from '../routes/toursRouter.js';
 import {errorHandler} from '../middleware/errorHandler.js';
+import {pcPartRoutes} from './routes/pcPartRoutes';
 
 export function startRoutes(app) {
   app.use('/api/v1/test', testRouter);
   //TODO: Everyone creates their own routes
   app.use('/api/v1/tours', toursRouter);
   app.use(errorHandler);
+  app.use('/api/v1/parts', pcPartRoutes);
 }
