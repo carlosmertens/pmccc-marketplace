125fbf5b95517d24c02e33a98f2e09dbea542706
Christopher Kuhn <135048573+BambyQT@users.noreply.github.com>
2024-04-09 10:32:42 +0200

    Update

---
+++ models/TourModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const tourSchema = new mongoose.Schema({
+  name: {type: String, trim: true, minlength: 1, maxlength: 50, required: true},
+  price: {type: Number, min: 100, max: 9999, required: true},
+  priceDiscount: {type: Number, dafault: 0},
+  duration: {type: Number, required: true},
+  maxGroupSize: {type: Number, min: 2, max: 50, required: true},
+  difficulty: {
+    type: String,
+    enum: {
+      values: ['easy', 'medium', 'hard'],
+      message: 'Difficulty is either: easy, medium or hard',
+    },
+    required: true,
+  },
+  ratings: {
+    average: {type: Number, min: 0, max: 10, default: 0},
+    count: {type: Number, min: 0, max: 10, default: 0},
+  },
+  summary: {
+    type: String,
+    trim: true,
+    minlength: 10,
+    maxlength: 250,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxlength: 1000,
+    default: 'Contact us for more information!',
+  },
+  startDates: [Date],
+  createdAt: {type: Date, default: Date.now},
+});
+
+export const TourModel = mongoose.model('tours', tourSchema);
--- routes/toursRouter.js
+++ routes/toursRouter.js
@@ -1,2 +1,21 @@
-// TODO:
-// Carlos starts here ...
+import {Router} from 'express';
+import {asyncWrapper} from '../middleware/asyncWrapper.js';
+import {controllers} from '../controllers/toursControllers.js';
+// import {validateParam} from '../middlewares/validateParam';
+
+export const toursRouter = Router();
+
+// TODO: Delete validateParam middleware function
+// toursRouter.param('id', validateParam);
+
+toursRouter
+  .route('/')
+  .get(asyncWrapper(controllers.getAllTours))
+  .post(asyncWrapper(controllers.createNewTour));
+
+toursRouter
+  .route('/:id')
+  .get(asyncWrapper(controllers.getTour))
+  .put(asyncWrapper(controllers.updateTour))
+  .patch(asyncWrapper(controllers.patchTour))
+  .delete(asyncWrapper(controllers.deleteTour));
