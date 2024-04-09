45593d54afe8d3b840eceabbd7c1b9651200c196
Christopher Kuhn <135048573+BambyQT@users.noreply.github.com>
2024-04-09 16:00:00 +0200

    String Updated

--- startup/startRoutes.js
+++ startup/startRoutes.js
@@ -2,13 +2,13 @@ import {testRouter} from '../routes/testRouter.js';
 import {toursRouter} from '../routes/toursRouter.js';
 import {errorHandler} from '../middleware/errorHandler.js';
 import {bookRouter} from '../routes/bookRouter.js';
-import {pcPartRoutes} from './routes/pcPartRoutes';
+import {pcPartRoutes} from './routes/pcPartRoutes.js';
 
 export function startRoutes(app) {
   app.use('/api/v1/test', testRouter);
   //TODO: Everyone creates their own routes
   app.use('/api/v1/tours', toursRouter);
   app.use('/api/v1/books', bookRouter);
-  app.use('/api/v1/parts', pcPartRoutes.js);
+  app.use('/api/v1/parts', pcPartRoutes);
   app.use(errorHandler);
 }
