220b5b1923b213b3595c7ef014ddfa14233179c3
Claudia <duquita96@gmail.com>
2024-04-09 11:30:08 +0200

    tested no.1 claudia

--- startup/startRoutes.js
+++ startup/startRoutes.js
@@ -1,10 +1,12 @@
 import {testRouter} from '../routes/testRouter.js';
 import {toursRouter} from '../routes/toursRouter.js';
 import {errorHandler} from '../middleware/errorHandler.js';
+import {bookRouter} from '../routes/bookRouter.js';
 
 export function startRoutes(app) {
   app.use('/api/v1/test', testRouter);
   //TODO: Everyone creates their own routes
   app.use('/api/v1/tours', toursRouter);
   app.use(errorHandler);
+  app.use('/api/v1/books', bookRouter);
 }
