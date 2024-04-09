38dcd31d6193980254621746edde43d13b33414c
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-04-09 09:57:26 +0200

    Create tour router, model, controllers & middleware

--- startup/startRoutes.js
+++ startup/startRoutes.js
@@ -1,6 +1,10 @@
-import { testRouter } from '../routes/testRouter.js';
+import {testRouter} from '../routes/testRouter.js';
+import {toursRouter} from '../routes/toursRouter.js';
+import {errorHandler} from '../middleware/errorHandler.js';
 
 export function startRoutes(app) {
   app.use('/api/v1/test', testRouter);
   //TODO: Everyone creates their own routes
+  app.use('/api/v1/tours', toursRouter);
+  app.use(errorHandler);
 }
