bd35f9151ba4d884b2e8d1d2a17464fffcf88392
maxi <Maximilian.Autrum42@gmx.de>
2024-04-10 13:17:08 +0200

    update

--- startup/startRoutes.js
+++ startup/startRoutes.js
@@ -9,7 +9,8 @@ export function startRoutes(app) {
   app.use('/api/v1/test', testRouter);
   //TODO: Everyone creates their own routes
   app.use('/api/v1/tours', toursRouter);
-  app.use(errorHandler);
   app.use('/api/v1/books', bookRouter);
+  app.use('/api/v1/parts', pcPartrouter);
+  app.use(errorHandler);
   app.use("/api/v1/video-games", videoGameRouter);
 }
