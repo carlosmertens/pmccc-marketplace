63703efcb926a9a9ddfc4e7ded67977e03f93b53
maxi <Maximilian.Autrum42@gmx.de>
2024-04-10 13:28:58 +0200

    update

--- startup/startRoutes.js
+++ startup/startRoutes.js
@@ -2,7 +2,7 @@ import {testRouter} from '../routes/testRouter.js';
 import {toursRouter} from '../routes/toursRouter.js';
 import {errorHandler} from '../middleware/errorHandler.js';
 import {bookRouter} from '../routes/bookRouter.js';
-import {pcPartrouter} from '../routes/pcPartRouter.js';
+
 import videoGameRouter from "../routes/VideoGamesRoutes.js";
 
 export function startRoutes(app) {
@@ -10,7 +10,7 @@ export function startRoutes(app) {
   //TODO: Everyone creates their own routes
   app.use('/api/v1/tours', toursRouter);
   app.use('/api/v1/books', bookRouter);
-  app.use('/api/v1/parts', pcPartrouter);
+
   app.use("/api/v1/video-games", videoGameRouter);
   app.use(errorHandler);
 }
