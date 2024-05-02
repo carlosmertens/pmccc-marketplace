6138f3cdf7fb4d7f7f0561c713a647e39edf06b7
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-05-02 10:17:28 +0200

    Refactor: Coors options

--- startup/startMiddleware.js
+++ startup/startMiddleware.js
@@ -5,7 +5,19 @@ import {logger} from '../middleware/logger.js';
 export function startMiddleware(app) {
   app.use(express.urlencoded({extended: true}));
   app.use(express.json());
-  app.use(cors());
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'Access-Control-Allow-Credentials',
+        'x-auth-token',
+      ],
+    })
+  );
   // app.use(express.static('public'));
   app.use(logger);
 }
