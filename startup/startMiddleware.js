5378be992c43e9bbbaed9a8e700d49a1ba516d4a
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-05-02 11:17:03 +0200

    Refactor: Change local host port

--- startup/startMiddleware.js
+++ startup/startMiddleware.js
@@ -7,7 +7,7 @@ export function startMiddleware(app) {
   app.use(express.json());
   app.use(
     cors({
-      origin: 'http://localhost:3000',
+      origin: 'http://localhost:5173',
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       allowedHeaders: [
