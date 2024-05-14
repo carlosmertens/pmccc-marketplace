fa9e3ccd79e66ba6db1fe05c90ebc838032bec79
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-05-14 14:26:35 +0200

    Start the review

--- routes/booksRouter.js
+++ routes/booksRouter.js
@@ -14,12 +14,12 @@ booksRouter
   .post([auth, admin], asyncWrapper(controllers.createNewBook));
 
 booksRouter
+  .route('/reviews')
+  .get(asyncWrapper((req, res) => res.send('Reviews requested!')));
+
+booksRouter
   .route('/:id')
   .get(asyncWrapper(controllers.getBook))
   .put([auth, admin], asyncWrapper(controllers.updateBook))
   .patch([auth, admin], asyncWrapper(controllers.patchBook))
   .delete([auth, admin], asyncWrapper(controllers.deleteBook));
-
-booksRouter
-  .route('/reviews')
-  .get(asyncWrapper((req, res) => res.send('Reviews requested!')));
