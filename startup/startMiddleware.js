45db3d217e1e485db5f3d6c3c3347d46649fa295
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-05-16 11:09:56 +0200

    Fix PATCH bug

--- startup/startMiddleware.js
+++ startup/startMiddleware.js
@@ -9,7 +9,7 @@ export function startMiddleware(app) {
     cors({
       origin: 'http://localhost:5173',
       credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
       allowedHeaders: [
         'Content-Type',
         'Authorization',
