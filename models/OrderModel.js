76e895e51a6f7a3f070d88ed38f7701dcc161ca7
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-05-27 14:08:43 +0200

    Add timestamp to order model

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -1,28 +1,31 @@
 import mongoose from 'mongoose';
 
-const OrderSchema = mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Users',
-  },
-  detail: [
-    {
-      productId: { type: String, required: true },
-      productType: { type: String, required: true },
-      name: { type: String, required: true },
-      price: { type: Number, required: true },
+const OrderSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Users',
     },
-  ],
-  totalPrice: { type: Number, required: true },
-  status: {
-    type: String,
-    enum: {
-      values: ['pending', 'complete'],
-      message: 'Options: pending | complete',
+    detail: [
+      {
+        productId: { type: String, required: true },
+        productType: { type: String, required: true },
+        name: { type: String, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    totalPrice: { type: Number, required: true },
+    status: {
+      type: String,
+      enum: {
+        values: ['pending', 'complete'],
+        message: 'Options: pending | complete',
+      },
+      default: 'pending',
     },
-    default: 'pending',
   },
-});
+  { timestamps: true }
+);
 
 const OrderModel = mongoose.model('Orders', OrderSchema);
 
